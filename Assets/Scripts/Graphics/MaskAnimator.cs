cc7d3f18e327c839eb75658726a7cfa11cb4acb3
Paolo Romano <paolo.romano@advepa.it>
2024-01-27 10:14:38 +0100

    Creata logica e dati per color set maschere

--- Assets/Scripts/Graphics/MaskAnimator.cs
+++ Assets/Scripts/Graphics/MaskAnimator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace Code.Graphics {
     public class MaskAnimator : MonoBehaviour {
         #region Public Variables
+        [SerializeField] private ColorSetSO[] m_colorSets;
         [SerializeField] private AnimationCurve m_laughterAnimation;
         [SerializeField] private EventReference m_laughterClipEvent;
 
@@ -14,11 +15,16 @@ namespace Code.Graphics {
 
         #region Private Variables
         private SkinnedMeshRenderer _meshRenderer;
+        private TrailRenderer _trailRenderer;
+
         private MaterialPropertyBlock _block;
+        private MaterialPropertyBlock _trailBlock;
 
         private bool _animatingLaughter;
 
         private static readonly int MatProp_Hue = Shader.PropertyToID("_Hue");
+        private static readonly int MatProp_MainColor = Shader.PropertyToID("_BaseColor");
+        private static readonly int MatProp_EmissionColor = Shader.PropertyToID("_EmissionColor");
         #endregion
 
         #region Properties
@@ -27,11 +33,30 @@ namespace Code.Graphics {
         #region Behaviour Callbacks
         private void Awake() {
             _meshRenderer = GetComponent<SkinnedMeshRenderer>();
+            _trailRenderer = GetComponentInChildren<TrailRenderer>();
 
             _block = new MaterialPropertyBlock();
-            _block.SetFloat(MatProp_Hue, 0f);
+            _block.SetFloat(MatProp_Hue, m_colorSets[0].ObjectHue);
+
+            _trailBlock = new MaterialPropertyBlock();
+            _trailBlock.SetColor(MatProp_MainColor, m_colorSets[0].TrailColor);
+            _trailBlock.SetColor(MatProp_EmissionColor, m_colorSets[0].TrailColor);
 
             _meshRenderer.SetPropertyBlock(_block);
+            _trailRenderer.SetPropertyBlock(_trailBlock);
+        }
+
+        [ContextMenu("Set Color/0")]
+        private void SetColorZero() => SetColorType(0);
+        [ContextMenu("Set Color/1")]
+        private void SetColorOne() => SetColorType(1);
+        [ContextMenu("Set Color/2")]
+        private void SetColorTwo() => SetColorType(2);
+
+        public void SetColorType(int id) {
+            var colorSet = m_colorSets[id];
+            SetHueDeg(colorSet.ObjectHue);
+            SetTrailColor(colorSet.TrailColor);
         }
         #endregion
 
@@ -48,28 +73,22 @@ namespace Code.Graphics {
             Debug.Log("Laughing\n");
         }
 
-        /// <summary>
-        ///     Set the <b>Hue</b> for the target renderer's property block.
-        /// </summary>
-        /// <param name="value">The hue offset (0 .. 1)</param>
-        public void SetHueVal(float value) {
-            _meshRenderer.GetPropertyBlock(_block);
-            _block.SetFloat(MatProp_Hue, (value - .5f) * 180f);
-            _meshRenderer.SetPropertyBlock(_block);
-        }
+        #endregion
 
-        /// <summary>
-        /// Set the <b>Hue</b> for the target renderer's property block.
-        /// </summary>
-        /// <param name="degrees">The hue offset in degrees (-180° .. 180°)</param>
-        public void SetHueDeg(float degrees) {
+        #region Private Methods
+        private void SetHueDeg(float degrees) {
             _meshRenderer.GetPropertyBlock(_block);
             _block.SetFloat(MatProp_Hue, degrees);
             _meshRenderer.SetPropertyBlock(_block);
         }
-        #endregion
 
-        #region Private Methods
+        private void SetTrailColor(Color color) {
+            _trailRenderer.GetPropertyBlock(_trailBlock);
+            _trailBlock.SetColor(MatProp_MainColor, color);
+            _trailBlock.SetColor(MatProp_EmissionColor, color);
+            _trailRenderer.SetPropertyBlock(_trailBlock);
+        }
+
         private IEnumerator LaughCO() {
             _animatingLaughter = true;
 

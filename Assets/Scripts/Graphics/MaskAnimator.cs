650271763ef43875bb4e5d931f9c69109747b38a
Paolo Romano <paolo.romano@advepa.it>
2024-01-27 01:43:33 +0100

    Aggiunti modelli Wakakas e vegetazione, Creata cutscene intro Wakakas

---
+++ Assets/Scripts/Graphics/MaskAnimator.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using FMODUnity;
+using UnityEngine;
+
+namespace Code.Graphics {
+    public class MaskAnimator : MonoBehaviour {
+        #region Public Variables
+        [SerializeField] private AnimationCurve m_laughterAnimation;
+        [SerializeField] private EventReference m_laughterClipEvent;
+
+        [Space]
+        [SerializeField] private int m_laughterShapeIndex;
+        #endregion
+
+        #region Private Variables
+        private SkinnedMeshRenderer _meshRenderer;
+        private MaterialPropertyBlock _block;
+
+        private bool _animatingLaughter;
+
+        private static readonly int MatProp_Hue = Shader.PropertyToID("_Hue");
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Behaviour Callbacks
+        private void Awake() {
+            _meshRenderer = GetComponent<SkinnedMeshRenderer>();
+
+            _block = new MaterialPropertyBlock();
+            _block.SetFloat(MatProp_Hue, 0f);
+
+            _meshRenderer.SetPropertyBlock(_block);
+        }
+        #endregion
+
+        #region Public Methods
+        [ContextMenu("Laugh")]
+        public void AnimateLaughter() {
+            if (!isActiveAndEnabled)
+                return;
+
+            if (_animatingLaughter)
+                return;
+
+            StartCoroutine(LaughCO());
+            Debug.Log("Laughing\n");
+        }
+
+        /// <summary>
+        ///     Set the <b>Hue</b> for the target renderer's property block.
+        /// </summary>
+        /// <param name="value">The hue offset (0 .. 1)</param>
+        public void SetHueVal(float value) {
+            _meshRenderer.GetPropertyBlock(_block);
+            _block.SetFloat(MatProp_Hue, (value - .5f) * 180f);
+            _meshRenderer.SetPropertyBlock(_block);
+        }
+
+        /// <summary>
+        /// Set the <b>Hue</b> for the target renderer's property block.
+        /// </summary>
+        /// <param name="degrees">The hue offset in degrees (-180° .. 180°)</param>
+        public void SetHueDeg(float degrees) {
+            _meshRenderer.GetPropertyBlock(_block);
+            _block.SetFloat(MatProp_Hue, degrees);
+            _meshRenderer.SetPropertyBlock(_block);
+        }
+        #endregion
+
+        #region Private Methods
+        private IEnumerator LaughCO() {
+            _animatingLaughter = true;
+
+            var t = 0f;
+            var duration = m_laughterAnimation.keys[^1].time;
+
+            if (!m_laughterClipEvent.IsNull) {
+                var ev = RuntimeManager.CreateInstance(m_laughterClipEvent);
+                ev.start();
+            }
+
+            while (t < duration) {
+                t += Time.deltaTime;
+                _meshRenderer.SetBlendShapeWeight(m_laughterShapeIndex, m_laughterAnimation.Evaluate(t) * 100);
+                yield return null;
+            }
+
+            _meshRenderer.SetBlendShapeWeight(m_laughterShapeIndex, 0f);
+            _animatingLaughter = false;
+        }
+        #endregion
+
+        #region Event Methods
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Miscellaneous/CutsceneIntroController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Miscellaneous {
+    [System.Serializable]
+    public class CreatureRef {
+        public GameObject creature;
+        public string name;
+    }
+
+    [RequireComponent(typeof(PlayableDirector))]
+    public class CutsceneIntroController : MonoBehaviour {
+        #region Public Variables
+        [SerializeField] private List<CreatureRef> m_creatures = new();
+        [SerializeField] private TextMesh m_text;
+        #endregion
+
+        #region Private Variables
+        private PlayableDirector _director;
+
+        private event System.Action _onCutsceneEnded;
+        #endregion
+
+        #region Behaviour Callbacks
+        private void Awake() {
+            _director = GetComponent<PlayableDirector>();
+            _director.playOnAwake = false;
+            _director.stopped += EndCutscene;
+
+            gameObject.SetActive(false);
+        }
+        #endregion
+
+        #region Public Methods
+        [ContextMenu("Play Cutscene")]
+        private void TestPlayback() => PlayCutscene(0, () => Debug.Log("DIO\n"));
+
+        public void PlayCutscene(int creatureIndex, System.Action onCutsceneEnded) {
+            if (_director.state == PlayState.Playing)
+                return;
+
+            for (var i = 0; i < m_creatures.Count; i++)
+                m_creatures[i].creature.SetActive(i == creatureIndex);
+            m_text.text = m_creatures[creatureIndex].name;
+
+            gameObject.SetActive(true);
+
+            _onCutsceneEnded = onCutsceneEnded;
+            _director.Play();
+        }
+        #endregion
+
+        #region Event Methods
+        private void EndCutscene(PlayableDirector director) {
+            _onCutsceneEnded?.Invoke();
+            _onCutsceneEnded = null;
+            gameObject.SetActive(false);
+        }
+        #endregion
+    }
+}

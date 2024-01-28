b2a46ab015d4eff33a3c6948ff61520ce12a20de
GiuliaSperandeo <73483522+GiuliaSperandeo@users.noreply.github.com>
2024-01-28 10:08:50 +0100

    build settings

--- Assets/Scripts/FMODEvents.cs
+++ Assets/Scripts/FMODEvents.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
-using Mono.Cecil;
 using EventReference = FMODUnity.EventReference;
 
 public class FMODEvents : MonoBehaviour

fea1c88f9317e7e6292d28319d6c880e79611349
Astaroth115 <peluso.lorenzo115@gmail.com>
2024-01-28 11:20:58 +0100

    Settings UI completo + altre cose

---
+++ Assets/Scripts/UI/SettingsComponentUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Code.UI
+{
+    public class SettingsComponentUI : MonoBehaviour
+    {
+        #region Public Variables   
+        public Toggle m_Toggle;
+        public Slider m_Slider;
+        public TMP_Text m_Text;
+        public bool IsOn;
+        #endregion
+    }
+}

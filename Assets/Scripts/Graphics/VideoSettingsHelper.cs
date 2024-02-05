7c5b9dfc69631e1eba24ecbc3bcd6b717bcda4c2
Aurora Corsi <aurora.corsi@advepa.it>
2024-02-05 23:27:10 +0100

    setup audio settings
    inseriti fields nel prefab UI dei settings, setup iniziale degli scripts

---
+++ Assets/Scripts/Graphics/VideoSettingsHelper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.Graphics
+{
+    public class VideoSettingsHelper
+    {
+        public static int MouseSensitivity = 30;
+        public static int FOV = 60;
+        public static bool MotionBlurActive = true;
+    }
+}
+
--- Assets/Scripts/UI/SettingsUI.cs
+++ Assets/Scripts/UI/SettingsUI.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using Code.Graphics;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,6 +13,9 @@ namespace Code.UI
         [SerializeField] private SettingsComponentUI m_sfx;
         [SerializeField] private SettingsComponentUI m_ui;
         [SerializeField] private SettingsComponentUI m_vo;
+        [SerializeField] private SettingsComponentUI m_sensitivity;
+        [SerializeField] private SettingsComponentUI m_fov;
+        [SerializeField] private SettingsComponentUI m_blur;
         [SerializeField] private Button m_backButton;
         #endregion
 
@@ -36,6 +37,9 @@ namespace Code.UI
             m_ui.m_Slider.onValueChanged.RemoveAllListeners();
             m_vo.m_Toggle.onValueChanged.RemoveAllListeners();
             m_vo.m_Slider.onValueChanged.RemoveAllListeners();
+            m_sensitivity.m_Slider.onValueChanged.RemoveAllListeners();
+            m_fov.m_Slider.onValueChanged.RemoveAllListeners();
+            m_blur.m_Toggle.onValueChanged.RemoveAllListeners();
             m_backButton.onClick.RemoveAllListeners();
         }
         private void Start()
@@ -56,6 +60,9 @@ namespace Code.UI
             Setup(m_sfx.m_Toggle, m_sfx.m_Slider, "bus:/SFX", m_sfx.IsOn, m_sfx.m_Text);
             Setup(m_ui.m_Toggle, m_ui.m_Slider, "bus:/UI", m_ui.IsOn, m_ui.m_Text);
             Setup(m_vo.m_Toggle, m_vo.m_Slider, "bus:/VO", m_vo.IsOn, m_vo.m_Text);
+            SettingsSetup(null, m_sensitivity.m_Slider, VideoSettingsHelper.MouseSensitivity, false, false, m_sensitivity.m_Text);
+            SettingsSetup(null, m_fov.m_Slider, VideoSettingsHelper.FOV, false, false, m_fov.m_Text);
+            SettingsSetup(m_blur.m_Toggle, null, 0, VideoSettingsHelper.MotionBlurActive, m_blur.IsOn, m_blur.m_Text);
             m_backButton.onClick.AddListener(DestroyThisGO);
         }
         private void Setup(Toggle myToggle, Slider mySlider, string busName, bool savedToggle, TMP_Text textValue)
@@ -72,6 +79,18 @@ namespace Code.UI
             mySlider.onValueChanged.AddListener(delegate
             {
                 textValue.text = UpdateVolume(mySlider.value, busName);
+            });
+        }
+        private void SettingsSetup(Toggle myToggle, Slider mySlider, int value, bool check, bool savedToggle, TMP_Text textValue)
+        {
+            if(mySlider) mySlider.value = value;
+            if(myToggle) myToggle.isOn = check;
+
+            myToggle.onValueChanged.AddListener(delegate
+            {
+            });
+            mySlider.onValueChanged.AddListener(delegate
+            {
             });
         }
         private bool ToggleVolume(bool isOn, string busName)

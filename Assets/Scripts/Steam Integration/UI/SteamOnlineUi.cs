1bae4b4ee03b0dac3241a1a03058b85ee9fc2822
Paolo Romano <paolo.fixp@gmail.com>
2024-06-02 18:31:17 +0200

    [FIX] Clear Steam Stats | [NEW] Steam Icon per verifica connessione

---
+++ Assets/Scripts/Steam Integration/UI/SteamOnlineUi.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SteamIntegration.UI {
+    public class SteamOnlineUi : UIBehaviour {
+        #region Public Variables
+        [SerializeField] private TMP_Text m_userNameText;
+        #endregion
+
+        #region Behaviour Callbacks
+        protected override void Awake() => m_userNameText.text = SteamManager.Singleton.User.Name;
+        #endregion
+    }
+}
